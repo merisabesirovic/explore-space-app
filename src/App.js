1e8c175412dd9e8c2dd2bea1c6d34b5645aad7f6
Merisa Besirovic <besiroviccm@gmail.com>
2023-06-13 22:55:57 +0200

    change

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import "./App.css";
-import { LocalizationProvider } from "@mui/x-date-pickers";
-import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import PlanetImages from "./pages/PlanetImages/PlanetImages";
 import MarsGenerator from "./pages/MarsGenerator/MarsGenerator";
 
