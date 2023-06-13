4b35dc9b86b638d5a58016b54adb63a7d5414f59
aladinzecic <aladinzecic10@gmail.com>
2023-06-13 22:14:27 +0200

    Navbar

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import { LocalizationProvider } from "@mui/x-date-pickers";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import PlanetImages from "./pages/PlanetImages/PlanetImages";
 import MarsGenerator from "./pages/MarsGenerator/MarsGenerator";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
     <div className="App">
       {" "}
+      <Navbar/>
       <PlanetImages></PlanetImages>
       <MarsGenerator></MarsGenerator>
     </div>
