3c832b8fc71ad2d1a964e4ec5b39cf0e3ab484b1
Merisa Besirovic <besiroviccm@gmail.com>
2023-06-13 22:59:49 +0200

    update

--- src/pages/MarsGenerator/MarsGenerator.js
+++ src/pages/MarsGenerator/MarsGenerator.js
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from "react";
-import { DatePicker } from "@mui/x-date-pickers";
+// import { DatePicker } from "@mui/x-date-pickers";
 import { LocalizationProvider } from "@mui/x-date-pickers";
 import dayjs from "dayjs";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import Pagination from "@mui/material/Pagination";
 import Stack from "@mui/material/Stack";
 import "./MarsGenerator.css";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 const KEY = "OIabT3zBh1iqsJkFE3T740jK32aDZRoV5CgWqC3D";
 
@@ -14,6 +16,17 @@ export default function MarsGenerator() {
   const today = dayjs().subtract(2, "days").format("YYYY-MM-DD");
   console.log(today);
   const [date, setDate] = useState(dayjs(today));
+  const [startDate, setStartDate] = useState(new Date());
+  startDate.setDate(startDate.getDate() - 2);
+  function formatDate(date) {
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, "0");
+    const day = String(date.getDate()).padStart(2, "0");
+
+    return `${year}-${month}-${day}`;
+  }
+  // console.log(formatDate(startDate));
+
   const [page, setPage] = useState(1);
   const handleChange = (event, value) => {
     setPage(value);
@@ -26,7 +39,9 @@ export default function MarsGenerator() {
     async function fetchPhoto() {
       try {
         const res = await fetch(
-          `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=${date.format(
+          `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=${formatDate(
+            startDate
+          )}
             "YYYY-MM-DD"
           )}&api_key=${KEY}`
         );
@@ -37,7 +52,7 @@ export default function MarsGenerator() {
         console.log(error);
       }
     }
-  }, [date]);
+  }, [startDate]);
   const numOfPages = Math.ceil((pictureData?.length || 0) / imagePerPage);
 
   return (
@@ -47,13 +62,20 @@ export default function MarsGenerator() {
         *Note that we cannot provide photos from today or tommorow{" "}
       </p>
       <div className="date-picker">
-        <LocalizationProvider dateAdapter={AdapterDayjs} locale="en">
+        {/* <LocalizationProvider dateAdapter={AdapterDayjs} locale="en">
           <DatePicker
             className="date-picker"
             value={date}
             onChange={(newValue) => setDate(newValue)}
           />
-        </LocalizationProvider>
+        </LocalizationProvider> */}
+        <DatePicker
+          className="date-picker"
+          showIcon
+          placeholderText="Enter a date"
+          selected={startDate}
+          onChange={(newValue) => setStartDate(newValue)}
+        />
       </div>
       {pictureData && pictureData.length > 0 ? (
         <div className="mars2">
