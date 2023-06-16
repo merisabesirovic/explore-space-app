d10f8aed7f92de18d16b81ab75b9374eb53a4b0f
Merisa Besirovic <besiroviccm@gmail.com>
2023-06-16 15:05:52 +0200

    fixed player

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,20 +1,20 @@
 import React from "react";
 import ReactPlayer from "react-player";
-import "./Home.css"; // Import the CSS file for styling
-
+import "./Home.css";
 const videoUrl =
-  "https://res.cloudinary.com/dp7iyoyad/video/upload/v1686872267/video_ft3ufd.mp4";
+  "https://res.cloudinary.com/dp7iyoyad/video/upload/v1686872191/video_am6ks4.mp4";
 
 export default function Home() {
   return (
     <div className="video-container">
       <ReactPlayer
-        url={videoUrl}
         playing={true}
+        url={videoUrl}
         controls={false}
+        loop={true}
+        muted={true}
         width="100%"
         height="100%"
-        loop
       />
       <div className="text-overlay">
         <h2 className="mars-title">Explore space with us!</h2>
